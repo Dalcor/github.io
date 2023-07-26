b3a47e1a265dc5b53565414b460b8f39a7dcf8ec
Viktor Peredera <vitia2282@gmail.com>
2023-07-26 21:59:34 +0300

    feat: remove imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '../styles/Home.module.scss'
 import {useState} from "react";
 import clsx from "clsx";
 
-const inter = Inter({ subsets: ['latin'] })
 
 type Article = {
   title: string,
