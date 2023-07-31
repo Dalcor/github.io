779e575204c2afa754beaf6fedcb060e98f82206
Dalcor <36812894+Dalcor@users.noreply.github.com>
2023-07-31 20:18:26 +0000

    Deploying to public from @ Dalcor/github.io@76e3e3b390d846527f397d8a048930ea31eb89d4 🚀

---
+++ _next/static/QNoJvlkrBq2stDgdBwf5n/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/9618c18deffbafa0.css","static/chunks/pages/index-cac30be37c57c751.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
