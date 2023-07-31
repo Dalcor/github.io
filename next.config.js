fa98dc89659c12c2576af8ce6acf34859f1e1461
Viktor Peredera <vitia2282@gmail.com>
2023-08-01 02:08:10 +0300

    feat: set base url back

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
   reactStrictMode: true,
-  basePath: isProd ? '/erc223' : undefined
+  basePath: isProd ? '/github.io' : undefined
 }
 
 module.exports = nextConfig
