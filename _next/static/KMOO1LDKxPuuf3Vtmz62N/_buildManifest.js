10ef0266c6f631c7887ebf1b64a548b58a813b2f
Dalcor <36812894+Dalcor@users.noreply.github.com>
2023-07-31 01:04:04 +0000

    Deploying to public from @ Dalcor/github.io@6e178f1f9b0f9b028bdb4294083cfa6cf5bf4d11 🚀

---
+++ _next/static/KMOO1LDKxPuuf3Vtmz62N/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/679190c85ebc384c.css","static/chunks/pages/index-36ea316261d8ed25.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/omme9gH7wAjsV7mZCth-k/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/679190c85ebc384c.css","static/chunks/pages/index-d77861ea8f9db51b.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
