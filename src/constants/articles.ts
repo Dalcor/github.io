0c5d868ce7d5608a6fd9af664310bfcfac1514b6
Viktor Peredera <vitia2282@gmail.com>
2023-07-31 23:27:29 +0300

    feat: add example article commit

--- src/constants/articles.ts
+++ src/constants/articles.ts
@@ -100,5 +100,10 @@ export const articles: IArticle[] = [
     title: "Known problems of ERC-20 standard.",
     description: "This article describes how bad ERC-20 is",
     url: "https://dexaran820.medium.com/known-problems-of-erc20-token-standard-e98887b9532c"
+  },
+  {
+    title: "Example of adding new article",
+    description: "This record is added in development purpose",
+    url: "https://google.com"
   }
 ];
